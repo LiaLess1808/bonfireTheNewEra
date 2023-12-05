4b003a4f58aa388c408e2e3c57149816ca265396
Nathália Lessa <nathaliacamposlessa1@gmail.com>
2023-12-05 20:51:28 -0300

    delete head

--- app/view/index.php
+++ app/view/index.php
@@ -12,14 +12,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="icon" href="assets/logo.jpeg">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="styles/variable.css">
-    <link rel="stylesheet" href="styles/styles.css">
-
     <title>Bonfire</title>
 
     </head>
